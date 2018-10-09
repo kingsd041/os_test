914102f24bdc7fd6db9c0c43c172a452274f3f1e
Noctis Lucis <urakiny@gmail.com>
2018-10-09 16:31:47 +0800

    Add executor to project.

---
+++ engine/__init__.py
@@ -0,0 +1,1 @@
+from engine.test_example import *
--- engine/test_example.py
+++ engine/test_example.py
@@ -1,11 +1,39 @@
 # coding=utf-8
 
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 from utils import *
 
+client = None
 
-def example():
-    test()
+
+def get_amazon_client():
+    global client
+    client = setup_amazon()
+    if client:
+        print('Init succeed')
+    else:
+        print('Init failed')
+
+
+def get_client():
+    global client
+    client = setup_os()
+    if client:
+        print('Init succeed')
+    else:
+        print('Init failed')
+
+
+def test_example():
+    command = 'sudo mkdir /etc/test/ && sudo chmod 777 /etc/test/ && cd /etc/test/'
+    try:
+        stdin, stdout, stderr = client.exec_command(command)
+
+        st = stdout.read()
+        pull_file()
+
+    except Exception as e:
+        raise e
+    else:
+        assert st is not None
+    finally:
+        client.close()
