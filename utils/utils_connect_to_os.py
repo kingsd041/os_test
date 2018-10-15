b1e8ee7cecf396945fa2745a4ee5adfc000a7ff7
Noctis Lucis <urakiny@gmail.com>
2018-10-15 15:29:21 +0800

    Change function name and add parameters to function.
    Change function's work flow

--- utils/utils_connect_to_os.py
+++ utils/utils_connect_to_os.py
@@ -4,28 +4,29 @@ import os
 import pytest
 import paramiko
 from config import *
+from utils_config import _get_hvm_config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+IP_OR_HOST = None
 
 
 # Function tips, It was defined that function can be used by a parameter. Init once time
 # @pytest.fixture(scope='session')
-def init_ssh_client():
+def init_ssh_client(ip):
     try:
         # TODO Need to check folder which was named logs
+        global IP_OR_HOST
+        IP_OR_HOST = ip
         paramiko.util.log_to_file(BASE_DIR + '/logs/ssh.log')
-        config = _get_os_config()
+        config = _get_hvm_config()
         key = paramiko.RSAKey.from_private_key_file(config.get('amazon_hvm').get('private_key_path'))
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        client.connect(hostname=config.get('amazon_os').get('ip'),
-                       username=config.get('amazon_os').get('username'),
+        client.connect(hostname=ip,
+                       username=config.get('amazon_hvm').get('username'),
                        pkey=key, allow_agent=False, look_for_keys=False)
-        command = 'uname -a'
-        stdin, stdout, stderr = client.exec_command(command)
-        st = stdout.read()
-        print(st.__repr__())
     except Exception as e:
         client.close()
         raise e
@@ -33,17 +34,14 @@ def init_ssh_client():
         return client
 
 
-def _get_os_config():
-    return hvm_configs
-
-
 def pull_file():
     try:
+        global IP_OR_HOST
         paramiko.util.log_to_file(BASE_DIR + '/logs/amazon_os.log')
-        config = _get_os_config()
-        key = paramiko.RSAKey.from_private_key_file(config.get('amazon_os').get('private_key_path'))
-        transport = paramiko.Transport(config.get('amazon_os').get('ip'), 22)
-        transport.connect(username=config.get('amazon_os').get('username'), pkey=key)
+        config = _get_hvm_config()
+        key = paramiko.RSAKey.from_private_key_file(config.get('amazon_hvm').get('private_key_path'))
+        transport = paramiko.Transport(IP_OR_HOST, 22)
+        transport.connect(username=config.get('amazon_hvm').get('username'), pkey=key)
         sftp = paramiko.SFTPClient.from_transport(transport)
         p = sftp.put(BASE_DIR + '/logs/amazon_os.log', '/etc/test/amazon_os.log')
         # sftp.get('remove_path', 'local_path')
