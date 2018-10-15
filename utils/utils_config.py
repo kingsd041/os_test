af0957fbdf03ebc5ecd74bc8b391615a6568a65f
Noctis Lucis <urakiny@gmail.com>
2018-10-15 15:29:53 +0800

    Add get ip tuple function

--- utils/utils_config.py
+++ utils/utils_config.py
@@ -4,4 +4,20 @@ from config import *
 
 
 def _get_hvm_config():
+    """
+    Get hvm config
+    :return:
+    """
     return hvm_configs
+
+
+def get_ip_tuple():
+    """
+
+    :return:
+    """
+    ip_tuple = hvm_configs.get('amazon_hvm').get('ip')
+    if not ip_tuple:
+        raise Exception('Config is None')
+    else:
+        return ip_tuple
