8970e05fef866cbb124e8579fad551b37ada24d7
Noctis Lucis <urakiny@gmail.com>
2018-10-16 18:05:30 +0800

    Change current work flow.

--- engine/test_example.py
+++ engine/test_example.py
@@ -5,25 +5,16 @@ from utils import *
 CLIENT = None
 
 
-def init_client(ip):
-    global CLIENT
-    client = init_ssh_client(ip)
-    CLIENT = client
-    if client:
-        return True
-    else:
-        return False
-
-
 def test_example():
     command = 'sudo mkdir /etc/test/ && sudo chmod 777 /etc/test/ && cd /etc/test/'
     try:
-        stdin, stdout, stderr = CLIENT.exec_command(command)
-
-        st = stdout.read()
+        ip_tuple = get_ip_tuple()
+        for ip in ip_tuple:
+            client = init_ssh_client(ip)
+            stdin, stdout, stderr = client.exec_command(command)
+            st = stdout.read()
+            # TODO It's necessary to add save logs to local file.
+            print(st)
+            client.close()
     except Exception as e:
         raise e
-    else:
-        assert st is not None
-    finally:
-        CLIENT.close()
