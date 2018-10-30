e4836146feefc3e82d10a95ab06eab5466344bed
Noctis Lucis <urakiny@gmail.com>
2018-10-30 09:21:26 +0800

    Modify test_example

--- engine/test_example.py
+++ engine/test_example.py
@@ -2,19 +2,11 @@
 
 from utils import *
 
-CLIENT = None
 
-
-def test_example():
-    command = 'sudo mkdir /etc/test/ && sudo chmod 777 /etc/test/ && cd /etc/test/'
-    try:
-        ip_tuple = get_ip_tuple()
-        for ip in ip_tuple:
-            client = init_ssh_client(ip)
-            stdin, stdout, stderr = client.exec_command(command)
-            st = stdout.read()
-            # TODO It's necessary to add save logs to local file.
-            print(st)
-            client.close()
-    except Exception as e:
-        raise e
+def test_example(init_virtual_machine):
+    command = 'hostname'
+    feed_back = 'rancher-test'
+    client = init_virtual_machine
+    client.sendline(command)
+    number = client.expect(feed_back, timeout=10)
+    assert (number == 0)
