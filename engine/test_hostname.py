347b53392d2c276587074d196a3901efa0f22aa7
Noctis Lucis <urakiny@gmail.com>
2018-10-31 15:22:02 +0800

    Add check 'etc/hostname'

--- engine/test_hostname.py
+++ engine/test_hostname.py
@@ -13,5 +13,9 @@ def test_hostname(ros_kvm):
     feed_back = 'rancher-test'
     client = ros_kvm(cloud_config='http://192.168.1.24/ros/cloud-config.yml')
     client.sendline(command)
-    number = client.expect(feed_back, timeout=10)
-    assert (number == 0)
+    number = client.expect(feed_back, timeout=20)
+
+    command_etc = 'cat /etc/hosts'
+    client.sendline(command_etc)
+    number_etc = client.expect(feed_back, timeout=20)
+    assert (number == 0 and number_etc == 0)
