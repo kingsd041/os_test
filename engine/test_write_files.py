af784426871c3ff8c0d8e70eca0921ffd064b4a7
kingsd041 <nicholas_ksd@hotmail.com>
2018-10-31 16:36:52 +0800

    add test_write_files.py

---
+++ engine/test_write_files.py
@@ -0,0 +1,32 @@
+# coding = utf-8
+# Create date: 2018-10-31
+# Author :Hailong
+
+
+def test_environment(ros_kvm):
+    """
+    Test case for check write files after rancher os has been installed succeed.
+    :param ros_kvm:
+    :return:
+    """
+    command = 'sudo cat /test | grep "console content"'
+    feed_back = 'console content'
+    client = ros_kvm(cloud_config='http://192.168.1.24/ros/test_write_files/cloud-config.yml')
+    client.sendline(command)
+    number = client.expect(feed_back, timeout=20)
+
+    command_b = 'sudo cat /test2 | grep "console content"'
+    feed_back_b = 'console content'
+    client.sendline(command_b)
+    number_b = client.expect(feed_back_b, timeout=20)
+
+    command_c = 'sudo system-docker exec ntp cat /test | grep "ntp content"'
+    feed_back_c = 'ntp content'
+    client.sendline(command_c)
+    number_c = client.expect(feed_back_c, timeout=20)
+
+    command_d = 'sudo system-docker exec syslog cat /test | grep "syslog content"'
+    feed_back_d = 'syslog content'
+    client.sendline(command_d)
+    number_d = client.expect(feed_back_d, timeout=20)
+    assert (number == 0 and number_b == 0 and number_c == 0 and number_d == 0)
