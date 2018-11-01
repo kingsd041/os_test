e335e3e9bc3fc0cd1ba0462aafbae5db20256cd3
kingsd041 <nicholas_ksd@hotmail.com>
2018-11-01 14:02:05 +0800

    Add test_sysctl.py and test_tls.py

---
+++ engine/test_sysctl.py
@@ -0,0 +1,22 @@
+# coding = utf-8
+# Create date: 2018-09-01
+# Author :Hailong
+
+#
+def test_sysctl(ros_kvm):
+    """
+    Test case for check sysctl after rancher os has been installed succeed.
+    :param ros_kvm:
+    :return:
+    """
+    command = 'sudo cat /proc/sys/kernel/domainname'
+    feed_back = 'test'
+    client = ros_kvm(cloud_config='http://192.168.1.24/ros/test_sysctl/cloud-config.yml')
+    client.sendline(command)
+    number = client.expect_exact(feed_back, timeout=10)
+
+    command_b = 'sudo cat /proc/sys/dev/cdrom/debug'
+    feed_back_b = '1'
+    client.sendline(command_b)
+    number_b = client.expect_exact(feed_back_b, timeout=20)
+    assert (number == 0 and number_b == 0)
---
+++ engine/test_tls.py
@@ -0,0 +1,17 @@
+# coding = utf-8
+# Create date: 2018-10-31
+# Author :Hailong
+
+
+def test_tls(ros_kvm):
+    """
+    Test case for check tls after rancher os has been installed succeed.
+    :param ros_kvm:
+    :return:
+    """
+    command = 'set -e -x && sudo ros tls gen && docker --tlsverify version'
+    feed_back = 'Server'
+    client = ros_kvm(cloud_config='http://192.168.1.24/ros/test_tls/cloud-config.yml')
+    client.sendline(command)
+    number = client.expect(feed_back, timeout=10)
+    assert (number == 0)
