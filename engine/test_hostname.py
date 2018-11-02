5e5f173f4ef86e2388e0eac7fada05fc61d9892b
Noctis Lucis <urakiny@gmail.com>
2018-11-02 10:54:24 +0800

    Add the verification of feed  back contents.

--- engine/test_hostname.py
+++ engine/test_hostname.py
@@ -3,19 +3,23 @@
 # Author :Bowen Lee
 
 
-def test_hostname(ros_kvm):
+def test_hostname(ros_kvm, cmd_opt):
     """
     Test case for check hostname after rancher os has been installed succeed.
     :param ros_kvm:
     :return:
     """
+
     command = 'hostname'
     feed_back = 'rancher-test'
-    client = ros_kvm(cloud_config='http://192.168.1.24/ros/cloud-config.yml')
+    client = ros_kvm(cloud_config='{url}/test_hostname.yml'.format(url=cmd_opt))
     client.sendline(command)
-    number = client.expect(feed_back, timeout=20)
+    number = client.expect(feed_back, timeout=20, searchwindowsize=None)
+    feed_back_content = str(client.after, encoding='utf-8')
 
     command_etc = 'cat /etc/hosts'
     client.sendline(command_etc)
-    number_etc = client.expect(feed_back, timeout=20)
-    assert (number == 0 and number_etc == 0)
+    number_etc = client.expect(feed_back, timeout=20, searchwindowsize=None)
+    feed_back_etc_content = str(client.after, encoding='utf-8')
+    assert ((number == 0 and feed_back == feed_back_content)
+            and (number_etc == 0 and feed_back == feed_back_etc_content))
