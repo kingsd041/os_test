ee7729b8605979f820b2f76bc51e06b061964838
Noctis Lucis <urakiny@gmail.com>
2018-11-02 10:49:53 +0800

    Add parameters to executor function.

--- executor.py
+++ executor.py
@@ -3,11 +3,18 @@
 # Author :Bowen Lee
 
 
+import os
+
 import pytest
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == '__main__':
 
     try:
-        pytest.main()
+        pytest.main(
+            ["-s",
+             BASE_DIR + '{script_package}'.format(script_package='/pycharm_project_473/engine/'),
+             '--cmd_opt={url}'.format(url='http://192.168.1.24/ros')])
     except Exception as e:
         print(e.args[0])
