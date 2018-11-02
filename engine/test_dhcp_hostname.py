6469628d7a42bd8f57ff5e983a4ec1632a94b6d5
Noctis Lucis <urakiny@gmail.com>
2018-11-02 15:30:12 +0800

    Add new testcase

---
+++ engine/test_dhcp_hostname.py
@@ -0,0 +1,25 @@
+# coding = utf-8
+# Create date: 2018-10-29
+# Author :Bowen Lee
+
+
+def test_dhcp_hostname(ros_kvm, cmd_opt):
+    """
+    Test case for check dncp hostname after rancher os has been installed succeed.
+    :param ros_kvm:
+    :return:
+    """
+
+    command = 'hostname'
+    feed_back = 'rancher'
+    client = ros_kvm(cloud_config='{url}/test_dncp_hostname.yml'.format(url=cmd_opt))
+    client.sendline(command)
+    number = client.expect(feed_back, timeout=20, searchwindowsize=None)
+    feed_back_content = str(client.after, encoding='utf-8')
+
+    command_etc = 'cat /etc/hosts'
+    client.sendline(command_etc)
+    number_etc = client.expect(feed_back, timeout=20, searchwindowsize=None)
+    feed_back_etc_content = str(client.after, encoding='utf-8')
+    assert ((number == 0 and feed_back == feed_back_content)
+            and (number_etc == 0 and feed_back == feed_back_etc_content))
