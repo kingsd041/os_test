8b33bd4b38550b9faa7fd0b9321cec2f940c5e6e
Noctis Lucis <urakiny@gmail.com>
2018-11-05 13:44:16 +0800

    FIx mistaks

--- engine/test_tls.py
+++ engine/test_tls.py
@@ -12,7 +12,7 @@ def test_tls(ros_kvm_with_paramiko, cmd_opt):
     """
     command = 'set -e -x && sudo ros tls gen && docker --tlsverify version'
     feed_back = 'Client'
-    client = ros_kvm_with_paramiko(cloud_config='{url}/test_tls'.format(url=cmd_opt))
+    client = ros_kvm_with_paramiko(cloud_config='{url}/test_tls.yml'.format(url=cmd_opt))
     stdin, stdout, stderr = client.exec_command(command, timeout=10)
     # Must be save in local constant
     output_content = stdout.read().decode('utf-8')
